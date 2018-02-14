af6742c15edcff5310f2633b9c8ac1fc01a7b9a8
Christopher Loonam <chrisloonam@gmail.com>
2018-02-14 00:16:22 -0500

    Simplification

--- src/sorting_main.c
+++ src/sorting_main.c
@@ -5,8 +5,10 @@
 #include "sorting.h"
 
 static void _free_node(Node *node) {
+	//frees a node and all its connected nodes
 	Node *temp = NULL;
 	while (node) {
+		//uses a while loop instead of recursion for memory purposes
 		temp = node;
 		node = node->next;
 		free(temp);
@@ -15,63 +17,62 @@ static void _free_node(Node *node) {
 
 int main(int argc, char **argv) {
 	if (argc != 4) {
+		//requires 4 arguments
 		return EXIT_FAILURE;
 	}
+	if (strcmp(argv[1], "-a") && strcmp(argv[1], "-l")) {
+		//invalid option
+		return EXIT_FAILURE;
+	}
+	int size = 0; //number of integers
+	double n_cmp = 0.0; //number of sorting comparisons
 
+	clock_t io = 0; //I/O time
+	clock_t sorting = 0; //sorting time
+	clock_t initial = clock(); //initial clock time
+
+	long *array = NULL;
+	Node *root = NULL;
 	if (strcmp(argv[1], "-a") == 0) {
-		int size = 0;
-		double n_cmp = 0.0;
-
-		clock_t io = 0;
-		clock_t sorting = 0;
-		clock_t initial = clock();
-		
-		long *array = Load_Into_Array(argv[2], &size);
-		io += clock()-initial;
-
+		//if -a, loads integers into array
+		array = Load_Into_Array(argv[2], &size);
 		if (!array) {
 			return EXIT_FAILURE;
 		}
-
-		initial = clock();
-		Shell_Sort_Array(array, size, &n_cmp);
-		sorting = clock()-initial;
-
-		initial = clock();
-		int saved = Save_From_Array(argv[3], array, size);
-		io += clock()-initial;
-		if (saved < size) {
-			return EXIT_FAILURE;
-		}
-
-		fprintf(stdout, "I/O time: %le\nSorting time: %le\nNumber of comparisons: %le\n", (double)io/CLOCKS_PER_SEC, (double)sorting/CLOCKS_PER_SEC, n_cmp);
-		free(array);
-		return EXIT_SUCCESS;
-	} else if (strcmp(argv[1], "-l") == 0) {
-		double n_cmp = 0.0;
-
-		clock_t io = 0;
-		clock_t sorting = 0;
-		clock_t initial = clock();
-		
-		Node *root = Load_Into_List(argv[2]);
+	} else {
+		//if -l, loads integers into linked list
+		root = Load_Into_List(argv[2]);
+		size = root->value; //gets the size from the value of the first node
 		if (!root) {
 			return EXIT_FAILURE;
 		}
-		io += clock()-initial;
-		
-		initial = clock();
-		Shell_Sort_List(root, &n_cmp);
-		sorting = clock()-initial;
-
-		initial = clock();
-		Save_From_List(argv[3], root);
-		io += clock()-initial;
-		
-		_free_node(root);
-		fprintf(stdout, "I/O time: %le\nSorting time: %le\nNumber of comparisons: %le\n", (double)io/CLOCKS_PER_SEC, (double)sorting/CLOCKS_PER_SEC, n_cmp);
-		return EXIT_SUCCESS;
 	}
+	io += clock()-initial;
 
-	return EXIT_FAILURE;
+	initial = clock();
+	//performs shell sort on the integers
+	if (array) {
+		Shell_Sort_Array(array, size, &n_cmp);
+	} else {
+		Shell_Sort_List(root, &n_cmp);
+	}
+	sorting = clock()-initial;
+
+	initial = clock();
+	//saves the sorted integers to a file
+	if (array) {
+		Save_From_Array(argv[3], array, size);
+	} else {
+		Save_From_List(argv[3], root);
+	}
+	io += clock()-initial;
+
+	fprintf(stdout, "I/O time: %le\nSorting time: %le\nNumber of comparisons: %le\n", (double)io/CLOCKS_PER_SEC, (double)sorting/CLOCKS_PER_SEC, n_cmp);
+	if (array) {
+		free(array);
+	} else {
+		_free_node(root);
+	}
+	
+	return EXIT_SUCCESS;
 }
